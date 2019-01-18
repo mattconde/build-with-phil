adc502b826ed4798b741e8666db9bc12d0767474
Matt Conde <matt.conde@gmail.com>
2019-01-18 13:03:31 +0000

    Build with phil: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
+const path = require("path");
 const context = process.cwd();
+const buildDir = path.resolve(context, "./build/");
 
 module.exports = {
-  context
+  context,
+  mode: "development",
+  entry: {
+    "main.js": require.resolve("./index")
+  },
+  resolve: {
+    alias: {
+      "scripts/app": require.resolve("./app")
+    },
+    symlinks: true,
+    extensions: [".js", ".json"]
+  },
+  output: {
+    path: buildDir,
+    filename: "[name]",
+    chunkFilename: "[name].js",
+    publicPath: "/_build/"
+  }
 };
